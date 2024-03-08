b36bb104a1c35ae8eafe84fa61d21213a7ee8314
iamtonmoy0 <tonmoy2code@gmail.com>
2024-03-08 16:20:56 +0600

    Add class-transformer and class-validator to package.json and package-lock.json Add LoggerMiddleware and ValidationPipe to the AppModule Add class-validator to the Product DTO Remove e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,32 @@
-import { Module } from '@nestjs/common';
+import {
+  Logger,
+  MiddlewareConsumer,
+  Module,
+  Req,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { LoggerMiddleware } from './logger/logger.middleware';
+import { ProductsController } from './products/products.controller';
 
 @Module({
   imports: [ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  // applying middleware for product controller
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'products', method: RequestMethod.GET });
+
+    // also we can use it for excluding for some method
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({ path: 'Products', method: RequestMethod.GET })
+      .forRoutes(ProductsController);
+  }
+}
--- src/products/dto/product.dto.ts
+++ src/products/dto/product.dto.ts
@@ -1,4 +1,10 @@
-export interface Product {
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class Product {
+  @IsNotEmpty()
+  @IsString()
   readonly id: string;
+
+  @IsString()
   readonly name: string;
 }
